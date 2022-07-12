8cbded7a81a4186c8e570eee0f523e29f470bd0e
Ivan Zaburdaev <ivanlom227@gmail.com>
2022-07-12 17:02:37 +0300

    cards-demo

---
+++ components/ItemCard.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import Image from "next/image";
+import {equipmentItem} from "./interfaces/equipmentItem";
+interface ItemCardInterface{
+    item:equipmentItem
+}
+const ItemCard = ({item}:ItemCardInterface) => {
+    let increase:any;
+    if(item.increase){
+        increase=Object.entries(item.increase);
+    }
+    let requirements:any;
+    if(item.requirements){
+        requirements=Object.entries(item.requirements);
+    }
+    let color:any;
+    switch (item.rarity){
+            case'common':
+                color='#B8B8B8';
+            case'uncommon':
+                color='#9BCA15';
+            case'epic':
+                color='#9600FF';
+            case'legendary':
+                color='#FF8F00';
+    }
+    let bg=' bg-['+color+']'
+    return (
+        <div className={'w-full h-full p-2 rounded-xl border-4 relative bg-uncommon-weak'}>
+            <div className={'w-28 h-28 relative mx-auto'}>
+                <div className={'w-full h-full absolute'}>
+                    <Image src={'/images/card_generation/item_bg.svg'} layout={'fill'}></Image>
+                </div>
+                <div className={'w-full h-full absolute'}>
+                    <Image src={'/images/miniatures/'+item.type+'/'+item.name+'.png'} layout={'fill'}></Image>
+                </div>
+            </div>
+            <p className={'text-center text-xl font-bold mb-4'}>{item.type}</p>
+            <div className={'grid grid-cols-2 gap-2'}>
+                <div className={'grid grid-cols-1 grid-rows-3 gap-2 h-28'}>
+                    {increase.map((incr:any)=>{
+                        console.log(incr[0]+incr[1]);
+                        return (<div className={'w-full h-full rounded-full flex justify-center items-center relative'} key={incr[0]}>
+                            <div className={'w-full h-full bg-white opacity-30 rounded-full absolute top-0 left-0'}></div>
+                            <div className={'w-8 h-8 rounded-full bg-white absolute left-0 p-2'}>
+                                <div className={'w-full h-full relative'}>
+                                    <Image src={'/images/card_generation/'+item.rarity+'/'+incr[0]+'.svg'} layout={'fill'}></Image>
+                                </div>
+                            </div>
+                            <p className={'absolute text-lg font-bold left-10'}>+{incr[1]}</p>
+                        </div>)
+                    })}
+                </div>
+                <div className={'grid grid-cols-1 grid-rows-3 h-4/5'}>
+                    <p className={'text-left text-xs font-bold'}>Require:</p>
+                    <div className={'grid grid-cols-2 gap-1'}>
+                        {requirements!=undefined? requirements.map((requirements:any)=>{
+                            return (<div className={'w-full h-5 rounded-full flex justify-center items-center relative'}>
+                                <div className={'w-full h-full bg-white opacity-30 rounded-full absolute top-0 left-0'}></div>
+                                <div className={'w-5 h-5 rounded-full bg-white absolute left-0 p-1'}>
+                                    <div className={'w-full h-full relative'}>
+                                        <Image src={'/images/card_generation/'+item.rarity+'/'+requirements[0]+'.svg'} layout={'fill'}></Image>
+                                    </div>
+                                </div>
+                                <p className={'absolute text-xs font-bold left-6'}>{requirements[1]}</p>
+                            </div>)
+                        }): null}
+                    </div>
+                    {item.rarity=='legendary'?<p className={'text-left text-xs font-medium'}>Status <strong>Elite</strong></p>:null}
+                </div>
+            </div>
+            <div className={'w-5 h-5 left-1 top-1 absolute'}>
+                <Image src={'/images/card_generation/'+item.type+'.svg'} layout={'fill'}></Image>
+            </div>
+            <p className={'h-5 right-2 w-20 text-right top-1 absolute text-sm'}>{item.id}</p>
+
+        </div>
+    );
+};
+
+export default ItemCard;
--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -4,12 +4,15 @@ import {workTask} from "./interfaces/workTask";
 const WorkCard = ({workItem}:workTask) => {
     return (
         <div className={'w-full h-full relative'}>
-            <div className={'w-full h-full relative rounded-full'}>
-                <Image src={'/images/m2e.png'} layout={'fill'} className={'rounded-2xl'}></Image>
+            <div className={'w-full h-full absolute -top-1 -left-2 rounded-full'}>
+                <Image src={'/images/m2e.png'} layout={'fill'} className={'rounded-3xl'}></Image>
             </div>
-            <svg width="" height="103%" viewBox="0 0 548 224" fill="none" xmlns="http://www.w3.org/2000/svg" className={'absolute top-0 '}>
-                <path transform="translate(-30,-5)" fillRule="evenodd" clipRule="evenodd" d="M360.662 78.5864C322.333 130.374 275.151 194.124 155 224H35.5417C15.9125 224 0 208.092 0 188.469V36.5311C0 16.9078 15.9125 1 35.5417 1H464.473C415.021 5.13988 390.529 38.2313 360.662 78.5864ZM548 36.5311V36.4892C548 27.0637 544.263 18.0242 537.612 11.3594C530.96 4.69451 521.939 0.950242 512.533 0.950242C502.236 0.950172 493.413 0.950195 484.198 0.950219L465.087 0.950242C464.882 0.966334 464.677 0.982921 464.473 1H512.458C532.088 1 548 16.9078 548 36.5311ZM548 188.461C548 194.607 546.411 200.588 543.462 205.854C537.373 216.684 525.77 224 512.458 224H155C155 224 155 224 155 224H512.573C521.973 223.989 530.983 220.24 537.626 213.577C539.934 211.261 541.891 208.659 543.462 205.854C546.352 200.715 548 194.784 548 188.469V188.461Z" fill="white"/>
-            </svg>
+            <div className={'w-full h-[110%] rounded-full absolute -top-2 -left-4'}>
+                <Image src={'/images/card_overlay.png'} layout={'fill'} className={'rounded-2xl'}></Image>
+            </div>
+            {/*<svg width="" height="103%" viewBox="0 0 548 224" fill="none" xmlns="http://www.w3.org/2000/svg" className={'absolute top-0 left-0 '}>*/}
+            {/*    <path transform="translate(-30,-5)" fillRule="evenodd" clipRule="evenodd" d="M360.662 78.5864C322.333 130.374 275.151 194.124 155 224H35.5417C15.9125 224 0 208.092 0 188.469V36.5311C0 16.9078 15.9125 1 35.5417 1H464.473C415.021 5.13988 390.529 38.2313 360.662 78.5864ZM548 36.5311V36.4892C548 27.0637 544.263 18.0242 537.612 11.3594C530.96 4.69451 521.939 0.950242 512.533 0.950242C502.236 0.950172 493.413 0.950195 484.198 0.950219L465.087 0.950242C464.882 0.966334 464.677 0.982921 464.473 1H512.458C532.088 1 548 16.9078 548 36.5311ZM548 188.461C548 194.607 546.411 200.588 543.462 205.854C537.373 216.684 525.77 224 512.458 224H155C155 224 155 224 155 224H512.573C521.973 223.989 530.983 220.24 537.626 213.577C539.934 211.261 541.891 208.659 543.462 205.854C546.352 200.715 548 194.784 548 188.469V188.461Z" fill="white"/>*/}
+            {/*</svg>*/}
             <div className={'w-full h-full p-4 absolute top-0'}>
                 <div className={'w-full h-full relative'}>
                     <div className={'absolute left-0 top-0'}>
