eecabe870218d284675080c80c58a77bf59c59a1
Ivan Zaburdaev <ivanlom227@gmail.com>
2022-08-15 22:16:04 +0300

    Merge branch 'prepare/metaforest_release' of C:\Users\Иван Забурдаев\Desktop\metaforest-mobile\metaforest-mobile-app with conflicts.

--- components/interfaces/bunnyInterface.ts
+++ components/interfaces/bunnyInterface.ts
@@ -2,43 +2,23 @@ import {equipmentItem} from "./equipmentItem";
 import {activeTask} from "./activeTask";
 
 export interface bunnyInterface{
-    bunny:{
-        id:number,
-        name:string,
-        lvl:number,
-        vitality:number,
-        carrots_limit:number,
-        energy:number,
-
-        stats:{
-            str:number,
-            dex:number,
-            vit:number,
-            int:number,
-            krm:number,
+    activeBunny: {
+        idx: number,
+        uid: string,
+        baseParams: {
+            int: number,
+            dex: number,
+            vit: number,
+            str: number,
+            krm: number,
+            level: number
         },
-
-        base:{
-            rabbit:string,
-            mouth?:string,
-            eyes?:string,
-            bg?:string,
-        },
-
-        equipment:{
-            overhead?:equipmentItem,
-            faces?:equipmentItem,
-            right?:equipmentItem,
-            left?:equipmentItem,
-            hats?:equipmentItem,
-            clothes?:equipmentItem,
-            necklace?:equipmentItem,
-            ears?:equipmentItem,
-        },
-        inventory:Array<equipmentItem>,
-        activeTask?:activeTask,
-        workHistory?:activeTask[],
-        isLevelUp:boolean;
+        images: {
+            original: string,
+            thumb: string,
+            web: string,
+            transparentBg: string
+        }
     },
 
 
