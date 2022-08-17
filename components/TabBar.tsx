cdee7e47e0ca670cd2f5fa48e801bdb54ebae4bb
Loty Gero <lotgyero@gmail.com>
2022-08-17 13:25:08 +0300

    FiX autoformat

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -1,56 +1,56 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {tabType} from "./types/tabType";
+import React, { useEffect, useRef, useState } from 'react';
+import { tabType } from "./types/tabType";
 import Image from "next/image";
-import {router} from "next/client";
+import { router } from "next/client";
 import Link from "next/link";
 
-interface tabBarInterface{
-    currentTab:string,
-    // UpdateTab:(newTab:tabType)=>any,
+interface tabBarInterface {
+  currentTab: string,
+  // UpdateTab:(newTab:tabType)=>any,
 }
 
-const TabBar = ({currentTab}:tabBarInterface) => {
+const TabBar = ({ currentTab }: tabBarInterface) => {
 
-    const [isVisible,setIsVisible]=useState('visible-true');
-    const [currentOffset,setCurrentOffset]=useState(0);
-    const tabs=[
-        'activity',
-        'equipment',
-        'home',
-        'marketplace',
-        'work',
-    ]
+  const [isVisible, setIsVisible] = useState('visible-true');
+  const [currentOffset, setCurrentOffset] = useState(0);
+  const tabs = [
+    'activity',
+    'equipment',
+    'home',
+    'marketplace',
+    'work',
+  ]
 
-    useEffect(()=>{
-        window.addEventListener('scroll',ev => {
-            if(window.scrollY>currentOffset+100){
-                setIsVisible('visible-false');
-                setCurrentOffset(window.scrollY);
-            }
-            else if (window.scrollY<currentOffset-100){
-                setIsVisible('visible-true');
-                setCurrentOffset(window.scrollY);
-            }
-        })
+  useEffect(() => {
+    window.addEventListener('scroll', ev => {
+      if (window.scrollY > currentOffset + 100) {
+        setIsVisible('visible-false');
+        setCurrentOffset(window.scrollY);
+      }
+      else if (window.scrollY < currentOffset - 100) {
+        setIsVisible('visible-true');
+        setCurrentOffset(window.scrollY);
+      }
     })
+  })
 
-    return (
-        <div className={'w-full h-full bg-white grid grid-cols-5 visible-transition gap-10 grid-rows-1 p-5 '+isVisible}>
-            {tabs.map(item=>{
-                let active='';
-                if('/'+item!=currentTab){
-                    active='_active'
-                }
-                return(
-                    <Link className={'relative'} href={'/'+item} key={item}>
-                        <div className={'relative'}>
-                            <Image src={'/images/tabs/'+item+active+'.svg'} layout={'fill'}></Image>
-                        </div>
-                    </Link>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className={'w-full h-full bg-white grid grid-cols-5 visible-transition gap-10 grid-rows-1 p-5 ' + isVisible}>
+      {tabs.map(item => {
+        let active = '';
+        if ('/' + item != currentTab) {
+          active = '_active'
+        }
+        return (
+          <Link className={'relative'} href={'/' + item} key={item}>
+            <div className={'relative'}>
+              <Image src={'/images/tabs/' + item + active + '.svg'} layout={'fill'}></Image>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default TabBar;
