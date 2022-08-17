fdda643fd1a2591db42b87e769ea6b44390f0b9f
Loty Gero <lotgyero@gmail.com>
2022-08-17 12:59:39 +0300

    Fix alt text and imports

--- components/BunnyGeneration.tsx
+++ components/BunnyGeneration.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useRef } from 'react';
 import Image from "next/image";
 import { random } from "nanoid";
 import { bunnyInterface } from "./interfaces/bunnyInterface";
+
 interface bunnyGenerationInterface {
   bunny: bunnyInterface
 }
-
 
 const BunnyGeneration = ({ bunny }: bunnyGenerationInterface) => {
   const baseList = Object.entries(bunny.bunny.base);
@@ -14,7 +14,9 @@ const BunnyGeneration = ({ bunny }: bunnyGenerationInterface) => {
     {baseList.map((item, counter) => {
       if (item[1] != '') {
         return <div key={random(4).toString()} className={'w-full absolute top-0 left-0 h-full'}>
-          <img src={'/images/bunny_generation/' + item[0] + '/' + item[1] + '.png'}
+          <Image
+            alt="bunny"
+            src={'/images/bunny_generation/' + item[0] + '/' + item[1] + '.png'}
             className={'rounded-xl'} />
         </div>
       } else {
@@ -25,7 +27,9 @@ const BunnyGeneration = ({ bunny }: bunnyGenerationInterface) => {
       // console.log(item);
       if (item[1].name != '') {
         return <div key={random(4).toString()} className={'w-full absolute top-0 left-0 h-full'}>
-          <img src={'/images/bunny_generation/' + item[1].type + '/' + item[1].name + '.png'}
+          <Image
+            alt="bunny"
+            src={'/images/bunny_generation/' + item[1].type + '/' + item[1].name + '.png'}
             className={'rounded-xl'} />
         </div>
       } else {
