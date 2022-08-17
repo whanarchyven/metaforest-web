c8d0f0120720525859bf069ba999f601a1242e45
Loty Gero <lotgyero@gmail.com>
2022-08-17 13:26:56 +0300

    Fix alt text

--- components/TopMenu.tsx
+++ components/TopMenu.tsx
@@ -10,12 +10,20 @@ const TopMenu = ({ balance }: topMenuInterface) => {
     <div className={'w-full h-full grid grid-cols-6 grid-rows-1 gap-2'}>
       <div className={'col-start-1 col-end-4 grid grid-cols-3 grid-rows-1'}>
         <div className={'relative col-start-1 col-end-1'}>
-          <Image src={'/images/burger_icon.svg'} layout={'fill'}></Image>
+          <Image
+            alt=""
+            src={'/images/burger_icon.svg'}
+            layout={'fill'}
+          ></Image>
         </div>
       </div>
       <div className={'col-start-4 col-end-6 justify-self-end'}>
         <div className={'h-full w-3 relative inline-block align-middle mr-2'}>
-          <Image src={'/images/carrot_icon.svg'} layout={'fill'}></Image>
+          <Image
+            alt=""
+            src={'/images/carrot_icon.svg'}
+            layout={'fill'}
+          ></Image>
         </div>
         <p className={'inline-block align-middle text-2xl font-semibold'}>
           {balance}
@@ -23,7 +31,11 @@ const TopMenu = ({ balance }: topMenuInterface) => {
       </div>
       <div className={'col-start-6 flex justify-center items-center col-end-6'}>
         <div className={'w-3/4 h-3/4 relative'}>
-          <Image src={'/images/wallet_icon.svg'} layout={'fill'}></Image>
+          <Image
+            alt=""
+            src={'/images/wallet_icon.svg'}
+            layout={'fill'}
+          ></Image>
         </div>
       </div>
     </div>
