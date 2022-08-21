59bb50b0efa5e8dd4d42babb937d9d7297a507b7
nickerlan <nickerlandev@gmail.com>
2022-08-21 03:36:10 +0300

    test

--- pages/echo.tsx
+++ pages/echo.tsx
@@ -4,12 +4,13 @@ import Script from "next/script";
 const PageEcho = () => {
   const [initData, setInitData] = useState("No init data");
   useEffect(() => {
-    setInitData((window as any)?.Telegram?.WebApp?.initData ?? "");
+    const data = (window as any)?.Telegram?.WebApp?.initData;
+    if (data) setInitData(data);
   }, []);
   return (
     <div>
       <Script src="https://telegram.org/js/telegram-web-app.js" />
-      <textarea value={initData} className={"w-full h-full"} />
+      <textarea value={initData} className={"w-full h-56"} />
     </div>
   );
 };
