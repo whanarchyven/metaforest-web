e7d5dbfe96c6d3920b8f0ef79fc6d8c19f10bd40
nickerlan <nickerlandev@gmail.com>
2022-08-21 03:18:54 +0300

    echo page

--- components/auth-connector.tsx
+++ components/auth-connector.tsx
@@ -7,17 +7,17 @@ import { useRouter } from "next/router";
 export const AuthConnector = () => {
   const Router = useRouter();
   useEffect(() => {
-    const testTgInitData =
-      "query_id=AAEvBg0AAAAAAC8GDQCti9A0&user=%7B%22id%22%3A853551%2C%22first_name%22%3A%22Nick%22%2C%22last_name%22%3A%22Erlan%22%2C%22username%22%3A%22nickolaierlan%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%7D&auth_date=1660076083&hash=7e75b96b1b650e545333d7053bc3b2826cb250ec5cf37d7a4c03e66538ddfa88";
+    const testTgInitData = localStorage.getItem("testTgInitData");
+    // "query_id=AAEvBg0AAAAAAC8GDQCti9A0&user=%7B%22id%22%3A853551%2C%22first_name%22%3A%22Nick%22%2C%22last_name%22%3A%22Erlan%22%2C%22username%22%3A%22nickolaierlan%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%7D&auth_date=1660076083&hash=7e75b96b1b650e545333d7053bc3b2826cb250ec5cf37d7a4c03e66538ddfa88";
 
     let telegramInitData = (window as any)?.Telegram?.WebApp?.initData;
+
     if (telegramInitData)
       localStorage.setItem("telegramInitData", telegramInitData);
 
     if (!telegramInitData) {
       telegramInitData = localStorage.getItem("telegramInitData");
-      console.log("test", Router.query.test);
-      if (!telegramInitData && Router.query.test)
+      if (!telegramInitData && testTgInitData)
         telegramInitData = testTgInitData;
       // alert('error: no init data')
     }
